93a39f845159f1fd1f37fb82b4d37a4bde15f83f
BoOodY <78202914+MrBoOodY@users.noreply.github.com>
2023-05-15 15:39:36 +0300

    Update sentry_service.dart

--- lib/common/core_data_source/sentry_service.dart
+++ lib/common/core_data_source/sentry_service.dart
@@ -5,7 +5,7 @@ class SentryService {
     await SentryFlutter.init(
       (options) {
         options.dsn =
-            'https://d0a797d93c29447482e86ec94e94182a@o4504708613996544.ingest.sentry.io/4504708614979584';
+            'Sentry Key';
 
         options.tracesSampleRate = 1.0;
       },
